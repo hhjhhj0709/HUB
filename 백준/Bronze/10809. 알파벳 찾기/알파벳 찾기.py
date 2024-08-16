e1dbc97c9645d3fafe86896edc472fac82342949
hhjhhj0709 <hhjhhj0709@gmail.com>
2024-08-16 13:45:37 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,8 @@
+text=input()
+al=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in al:
+    if i in text:
+        print(text.index(i),end=" ")
+    else:
+        print("-1",end=" ")
